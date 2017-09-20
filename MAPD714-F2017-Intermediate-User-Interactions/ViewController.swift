862430d6b7b4b242cd3de26ed2784598df350ec4
Sergey Kozak <Si@deep-book.local>
2017-09-20 19:31:24 -0400

    Initial Commit

---
+++ MAPD714-F2017-Intermediate-User-Interactions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD714-F2017-Intermediate-User-Interactions
+//
+//  Created by Sergey Kozak on 20/09/2017.
+//  Copyright © 2017 Centennial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

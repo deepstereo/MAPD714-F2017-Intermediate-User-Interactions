42665156ea85de48965c56fa2539564f72305e98
Sergey Kozak <Si@deep-book.local>
2017-09-20 20:16:07 -0400

    Original UI created

--- MAPD714-F2017-Intermediate-User-Interactions/ViewController.swift
+++ MAPD714-F2017-Intermediate-User-Interactions/ViewController.swift
@@ -5,14 +5,22 @@
 //  Created by Sergey Kozak on 20/09/2017.
 //  Copyright © 2017 Centennial. All rights reserved.
 //
+//  Intermediate User Interaction demo project
+//  Version 0.1 - Build basic UI
 
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
